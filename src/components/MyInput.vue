1e87f8b36340a1aa01a5875c17743037ea0cb7cc
Cassio Cabral <cassioscabral@gmail.com>
2016-04-21 17:38:40 -0300

    initial input to add new entries

---
+++ src/components/MyInput.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="my-input">
+    <mdl-textfield label="New Entry" floating-label="New Entry" :value.sync="entry"></mdl-textfield>
+    <mdl-button v-mdl-ripple-effect @click="addEntry">Add</mdl-button>
+  </div>
+</template>
+<script>
+import {
+  MdlButton,
+  MdlTextfield
+} from 'vue-mdl'
+// TODO
+// add props and make the name generic
+export default {
+  components: {
+    MdlButton,
+    MdlTextfield
+  },
+  data () {
+    return {
+      // note: changing this line won't causes changes
+      // with hot-reload because the reloaded component
+      // preserves its current state and we are modifying
+      // its initial state.
+      entry: ''
+    }
+  },
+  methods: {
+    addEntry () {
+      window.alert(this.entry)
+    }
+  }
+
+}
+</script>
+<style scoped>
+h1 {
+  color: #42b983;
+}
+</style>
